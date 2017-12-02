aa7eeffa992451f8761b3d16602ab6e89a95d731
Pavel Goultiaev <pavel.goultiaev@gmail.com>
2017-12-02 12:28:23 +0100

    improved Day 2

--- 2/2.go
+++ 2/2.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -18,52 +19,20 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var checksumPerLine1 []int
-	var checksumPerLine2 []int
+	sum1 := 0
+	sum2 := 0
 	for scanner.Scan() {
 		slicedString := strings.Split(scanner.Text(), "\t")
 
 		slicedInts := convStringArrayToIntArray(slicedString)
-		checksumPerLine1 = append(checksumPerLine1, max(slicedInts)-min(slicedInts))
-		checksumPerLine2 = append(checksumPerLine2, findDivisible(slicedInts))
+		sort.Ints(slicedInts[:])
+		fmt.Printf("%v", slicedInts)
+		sum1 += (slicedInts[len(slicedInts)-1] - slicedInts[0])
+		sum2 += findDivisible(slicedInts)
 	}
 
-	// Total checksum part one
-	total1 := 0
-	for _, v := range checksumPerLine1 {
-		total1 += v
-	}
-	fmt.Printf("part one: %d\n", total1)
-
-	// Total checksum part two
-	total2 := 0
-	for _, v := range checksumPerLine2 {
-		total2 += v
-	}
-	fmt.Printf("part two: %d\n", total2)
-
-}
-
-func max(slice []int) int {
-	max := 0
-	for _, e := range slice {
-		if e > max {
-			max = e
-		}
-	}
-	return max
-}
-
-func min(slice []int) int {
-	min := slice[0]
-	if len(slice) > 1 {
-		for _, e := range slice {
-			if e < min {
-				min = e
-			}
-		}
-	}
-	return min
+	fmt.Printf("part one: %d\n", sum1)
+	fmt.Printf("part two: %d\n", sum2)
 }
 
 func convStringArrayToIntArray(sa []string) []int {
