6bb4d0216ed9fa8d2012536500b78227f414babb
Pavel Goultiaev <pavel.goultiaev@gmail.com>
2017-12-06 06:43:25 +0100

    Day 5 slighlty optimised

--- 5/5.go
+++ 5/5.go
@@ -17,9 +17,22 @@ func main() {
 	slicedString := strings.Split(s, "\n")
 	slicedInts := convStringArrayToIntArray(slicedString)
 
-	fmt.Printf("part one: %d\n", solvePartOne(slicedInts))
-	fmt.Printf("part two: %d\n", solvePartTwo(convStringArrayToIntArray(slicedString)))
+	si2 := make([]int, len(slicedInts))
+	copy(si2, slicedInts)
 
+	//fmt.Printf("part one: %d\n")
+	//fmt.Printf("part two: %d\n")
+
+	messages := make(chan int)
+	go func() {
+		messages <- solvePartOne(slicedInts)
+	}()
+	go func() {
+		messages <- solvePartTwo(si2)
+	}()
+	for i := 0; i < 2; i++ {
+		fmt.Println(<-messages)
+	}
 }
 
 func solvePartOne(ia []int) int {
@@ -41,25 +54,24 @@ func solvePartOne(ia []int) int {
 	return count1
 }
 
-func solvePartTwo(ia []int) int {
-	count1 := 0
+func solvePartTwo(ia []int) (count1 int) {
 	i := 0
-	for i < len(ia) {
+	l := len(ia)
+	for i < l {
 		offset := ia[i]
 
 		count1++
 
 		if offset >= 3 {
-			ia[i] = ia[i] - 1
+			ia[i] = offset - 1
 		} else {
-			ia[i] = ia[i] + 1
+			ia[i] = offset + 1
 		}
 
 		i += offset
-		if i > len(ia) {
+		if i > l {
 			break
 		}
-
 		//fmt.Printf("%v\n", ia)
 	}
 	return count1
