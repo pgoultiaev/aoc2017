b9c6534fce46c2001774d1b7c2ac72c9ecc1d580
Pavel Goultiaev <pavel.goultiaev@gmail.com>
2017-12-21 17:25:57 +0100

    Day 9, fixed test cases

--- 9/9_test.go
+++ 9/9_test.go
@@ -4,27 +4,32 @@ import "testing"
 
 func Test_findSumGroups(t *testing.T) {
 	tests := []struct {
-		name string
-		text string
-		want int
+		name        string
+		text        string
+		wantSum     int
+		wantGarbage int
 	}{
-		{"unbalanced a", "{{}{}", 2},
-		{"unbalanced b", "{}{}}", 2},
-		{"unbalanced c", "{{{}{}}", 5},
-		{"1", "{}", 1},
-		{"2", "{{{}}}", 6},
-		{"3", "{{},{}}", 5},
-		{"4a", "{}{{}}", 4},
-		{"4", "{{{},{},{{}}}}", 16},
-		{"5", "{<a>,<a>,<a>,<a>}", 1},
-		{"6", "{{<ab>},{<ab>},{<ab>},{<ab>}}", 9},
-		{"7", "{{<!!>},{<!!>},{<!!>},{<!!>}}", 9},
-		{"8", "{{<a!>},{<a!>},{<a!>},{<ab>}}", 3},
+		{"unbalanced a", "{{}{}", 2, 0},
+		{"unbalanced b", "{}{}}", 2, 0},
+		{"unbalanced c", "{{{}{}}", 5, 0},
+		{"1", "{}", 1, 0},
+		{"2", "{{{}}}", 6, 0},
+		{"3", "{{},{}}", 5, 0},
+		{"4a", "{}{{}}", 4, 0},
+		{"4", "{{{},{},{{}}}}", 16, 0},
+		{"5", "{<a>,<a>,<a>,<a>}", 1, 4},
+		{"6", "{{<ab>},{<ab>},{<ab>},{<ab>}}", 9, 8},
+		{"7", "{{<!!>},{<!!>},{<!!>},{<!!>}}", 9, 0},
+		{"8", "{{<a!>},{<a!>},{<a!>},{<ab>}}", 3, 17},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findSumGroups(tt.text); got != tt.want {
-				t.Errorf("findSumGroups() = %v, want %v", got, tt.want)
+			gotSum, gCount := findSumGroups(tt.text)
+			if gotSum != tt.wantSum {
+				t.Errorf("findSumGroups() = %v, want %v", gotSum, tt.wantSum)
+			}
+			if gCount != tt.wantGarbage {
+				t.Errorf("findSumGroups() = %v, want %v", gCount, tt.wantGarbage)
 			}
 		})
 	}
